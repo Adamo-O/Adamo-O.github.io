de428a029b606e4503d4ce933b6cb6633845df8b
Adamo Orsini <adamo.orsini01@gmail.com>
2022-08-26 19:03:47 -0400

    Added navbar, and PTC experience

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Swap acceleration image on hover
 accelPosition = false;
 const accelSwitch = (img) => {
     if (!accelPosition) {
@@ -8,4 +9,23 @@ const accelSwitch = (img) => {
         img.src = "Images/L Acceleration.png";
         accelPosition = false;
     }
+}
+
+// Highlight section in navbar on visit
+current = '';
+window.onscroll = () => {
+    
+    document.querySelectorAll('.navSection').forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop <= 100) {
+            current = section.id;
+        }
+    });
+
+    document.querySelectorAll('.nav-link').forEach(linkItem => {
+        linkItem.classList.remove('active');
+        if (linkItem.getAttribute('href').includes(current)) {
+            linkItem.classList.add('active');
+        }
+    })
 }
