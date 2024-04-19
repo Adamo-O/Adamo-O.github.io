73a613bd6d7c4a0c191b415567ad22761ad39b63
Adamo Orsini <adamo.orsini01@gmail.com>
2024-04-18 22:02:04 -0400

    Fixed project and experience sorting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
 import { type ClassValue, clsx } from "clsx"
+import moment from "moment";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function sortContent(datesA: string, datesB: string) {
+  const aDates = datesA.split(" - ") as string[];
+  const aDate = aDates.length > 1 ? aDates[1] : aDates[0];
+  const bDates = datesB.split(" - ") as string[];
+  const bDate = bDates.length > 1 ? bDates[1] : bDates[0];
+
+  const diff = moment(bDate, 'MMM YYYY').diff(moment(aDate, 'MMM YYYY'));
+  if (diff === 0) {
+    if (aDates.length > 1 && bDates.length > 1) {
+      return moment(bDates[0], 'MMM YYYY').diff(moment(aDates[0], 'MMM YYYY'));
+    }
+    return moment(bDates[0], 'MMM YYYY').diff(moment(aDates[0], 'MMM YYYY'));
+  }
+
+  return diff;
 }
